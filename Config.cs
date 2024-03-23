4cf556cd1b3c71561a24644ce0b86861b61d7e49
Greyzer_SL <140040398+PHANTOMSCPSL@users.noreply.github.com>
2024-03-23 09:16:08 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,14 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace CustomHint
+{
+    public class Config : IConfig
+    {
+        [Description("This Plugin Enabled?")] public bool IsEnabled { get; set; } = true;
+        [Description("Plugin debug?")] public bool Debug { get; set; } = false;
+
+        [Description("Hint message, {tps}, {MinPlayers}, {MaxPlayers}, {Minutes}, {Seconds}, {ServerName}, {SCPs}...")]
+        public string Hint { get; set; } = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n<align=left>\n         \ud83d\udd51 | <color=#ffffff>The round lasts - {Minutes}:{Seconds}</color>\n         \ud83d\udc65 | <color=#ffffff>Players: {MinPlayers}/{MaxPlayers}</color>\n         \ud83d\udcdf | <color=#ffffff>TPS: {tps}</color>\n         \ud83c\udfae | <color=#ffffff>The server: {ServerName}</color>";
+    }
+}
